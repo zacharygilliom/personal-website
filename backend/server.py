e4f190cf051bf562341d106b5346b89abf6fa53b
zacharygilliom <zacharygilliom@gmail.com>
2021-11-23 00:03:52 -0500

    Updated github api request to only pull starred repos from repo

--- backend/server.py
+++ backend/server.py
@@ -1,23 +1,23 @@
-from flask import Flask
 import requests
+from flask import Flask
+from github import Github
+from configs.configs import Config
 
 app = Flask(__name__)
 
-@app.route('/')
-def get_user_repos():
-    r = requests.get('https://api.github.com/users/zacharygilliom/repos')
+g = Github(Config.github_access_token)
+
+@app.route('/get_my_repos')
+def get_user_repos(methods=['GET']):
     repos = {}
-    for repo in r.json():
-        repo_name = repo["name"]
-        repo_url = repo["html_url"]
-        print(repo['languages_url'])
-        repo_languages = repo["languages_url"]
-        r_l = requests.get(repo_languages)
+    for repo in g.get_user().get_starred():
+        repo_name = repo.name
+        repo_url = repo.html_url
         langs = []
-        for lang, num in r_l.json().items():
+        for lang, num in repo.get_languages().items():
             langs.append(lang) 
         repos[repo_name] = {"url":repo_url,
-                            "lang":langs}
+                                "lang":langs}
     return repos 
 
 if __name__ == "__main__":
